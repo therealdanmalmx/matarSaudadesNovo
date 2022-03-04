ba24cb8cd73afb0191b47bc26f772427fec2ead4
therealdanmalmx <danmalmx@gmail.com>
2022-03-04 20:39:51 +0100

    Remove component import and use

--- pages/categorias.js
+++ pages/categorias.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { getCollections } from '../lib/Shopify';
-import CategoryCard from '../components/CategoryCard';
 
 function Categories({ collections }) {
 
@@ -11,7 +10,6 @@ function Categories({ collections }) {
 			<h2 className="text-2xl text-center font-extrabold text-gray-900 mb-12">Categorias</h2>
         <div className="container max-w-7xl mx-auto flex justify-between cursor-pointer">
           {collections.map((collection) => (
-            // <CategoryCard key={collection.node.id} product={collection}/>
             <Link 
               href={`/categorias/${collection.node.handle}`}
               passHref
