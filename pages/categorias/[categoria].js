b89ac7d4c91cf18b9de9b07f3eb4937b9715c1c9
therealdanmalmx <danmalmx@gmail.com>
2022-03-08 21:02:53 +0100

    Fix key

--- pages/categorias/[categoria].js
+++ pages/categorias/[categoria].js
@@ -55,7 +55,7 @@ const Categoria = ({ categoria }) => {
                 
                 return (
                     <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 xl:gap-x-8">
-                        <ProductCard key={product.node.id} product={product.node}/> 
+                        <ProductCard key={prod.id} product={product.node}/> 
                     </div>
                 )
             })}
