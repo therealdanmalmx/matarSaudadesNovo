b8c12e58cc31654bd9191abeae3c0d035f89a91b
therealdanmalmx <danmalmx@gmail.com>
2022-03-10 22:54:51 +0100

    Centering categories

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,24 @@
-import Link from 'next/link'
-import Categorias from '../components/Categorias'
-import { getCollections } from './../lib/Shopify';
-import useTranslation from 'next-translate/useTranslation'
-
+import Link from "next/link";
+import Categorias from "../components/Categorias";
+import { getCollections } from "./../lib/Shopify";
+import useTranslation from "next-translate/useTranslation";
 
 function Home({ collections }) {
-
   let { t } = useTranslation();
 
   return (
-    <div className="text-center justify-start items-start">
-      <Categorias collections={collections}/>
+    <div className="text-center">
+      <Categorias collections={collections} />
     </div>
-  )
+  );
 }
 
 export const getStaticProps = async () => {
-  const collections = await getCollections()
+  const collections = await getCollections();
 
   return {
     props: { collections }, // will be passed to the page component as props
-  }
-}
+  };
+};
 
-export default Home
+export default Home;
