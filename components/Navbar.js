6bd3933f11557cd394778a192df10717e09fd225
therealdanmalmx <danmalmx@gmail.com>
2022-08-16 18:31:07 +0200

    Refactor

--- components/Navbar.js
+++ components/Navbar.js
@@ -28,17 +28,17 @@ export default function Navbar() {
           <div className="header-nav-wrapper flex-auto text-right">
             <ul className="inline-flex">
               <Link href="/" passHref>
-                <a className="border-b-4 border-transparent px-4 transition duration-300 focus:border-b-4 focus:border-red focus:text-red  focus:ease-in-out active:text-red">
+                <a className="border-transparent border-b-4 px-4 transition duration-300 focus:border-b-4 focus:border-red focus:text-red  focus:ease-in-out active:text-red">
                   {t("menu.home")}
                 </a>
               </Link>
               <Link href="/about" passHref>
-                <a className="border-b-4 border-transparent px-4 transition duration-300 focus:border-b-4 focus:border-red focus:text-red  focus:ease-in-out active:text-red">
+                <a className="border-transparent border-b-4 px-4 transition duration-300 focus:border-b-4 focus:border-red focus:text-red  focus:ease-in-out active:text-red">
                   {t("menu.about")}
                 </a>
               </Link>
               <Link href="/product" passHref>
-                <a className="border-b-4 border-transparent px-4 transition duration-300 focus:border-b-4 focus:border-red focus:text-red  focus:ease-in-out active:text-red">
+                <a className="border-transparent border-b-4 px-4 transition duration-300 focus:border-b-4 focus:border-red focus:text-red  focus:ease-in-out active:text-red">
                   {t("menu.product")}
                 </a>
               </Link>
@@ -63,12 +63,12 @@ export default function Navbar() {
           <div className="header-nav-wrapper flex-auto text-left">
             <ul className="inline-flex">
               <Link href="/news" passHref>
-                <a className="border-b-4 border-transparent px-4 transition duration-300 focus:border-b-4 focus:border-red-600 focus:text-red-600  focus:ease-in-out active:text-red-600">
+                <a className="border-transparent focus:border-red-600 focus:text-red-600 active:text-red-600 border-b-4 px-4 transition duration-300  focus:border-b-4 focus:ease-in-out">
                   {t("menu.news")}
                 </a>
               </Link>
               <Link href="/contacts" passHref>
-                <a className="border-b-4 border-transparent px-4 transition duration-300 focus:border-b-4 focus:border-red-600 focus:text-red-600  focus:ease-in-out active:text-red-600">
+                <a className="border-transparent focus:border-red-600 focus:text-red-600 active:text-red-600 border-b-4 px-4 transition duration-300  focus:border-b-4 focus:ease-in-out">
                   {t("menu.contacts")}
                 </a>
               </Link>
@@ -78,11 +78,11 @@ export default function Navbar() {
             <HeartIcon className="h-6 w-6" />
           </div>
           <div className="header-cart">
-            <ShoppingBagIcon 
+            <ShoppingBagIcon
               className="h-6 w-6"
-              onClick={() => setCartOpen(!cartOpen)} 
+              onClick={() => setCartOpen(!cartOpen)}
             />
-            <MiniCart cart={cart} />
+            {/* <MiniCart cart={cart} /> */}
           </div>
         </div>
 
