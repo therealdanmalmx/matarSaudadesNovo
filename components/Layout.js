1b0b2f33944d7827d1dbd1f99484ddb299239111
Sérgio Costa <costa.sergio.86@gmail.com>
2022-08-23 22:33:53 +0100

    Refactoring Footer

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex min-h-screen flex-col justify-between">
+    <div className="flex w-full min-h-screen flex-col justify-between">
       <Navbar />
       <main>{children}</main>
       <Footer />
