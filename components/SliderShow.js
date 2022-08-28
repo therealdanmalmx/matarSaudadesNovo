d7358110802e4d8c6f77510fbf51d03f7f722886
Sérgio Costa <costa.sergio.86@gmail.com>
2022-08-28 20:35:01 +0100

    Implement SlideShow React Slick

--- components/SliderShow.js
+++ components/SliderShow.js
@@ -1,41 +1,83 @@
 import { useState } from "react";
 import { HiOutlineChevronLeft, HiOutlineChevronRight } from "react-icons/hi";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SliderShow = ({ slides }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+ /*  const [currentIndex, setCurrentIndex] = useState(0);
+  const [activeSlide, setActiveSlide] = useState(0);
 
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
+    console.log("Previous", newIndex);
   };
 
   const goToNext = () => {
     const isFirstSlide = currentIndex === slides.length - 1;
     const newIndex = isFirstSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
+    console.log("Next", newIndex);
   };
 
-  return (
-    <div>
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  } */
+
+  const settings = {
+    infinite: true,
+    fade: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: true,
+    autoplay: true,
+    autoplaySpeed: 6000,  
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
       <div
-        className="relative mx-auto flex flex-col items-center justify-center bg-cover bg-center px-10 py-10 text-center md:py-36 lg:h-screen lg:max-w-full"
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-      >
-        <div
-          className="absolute top-2/4 left-0 z-50 translate-x-0 -translate-y-2/4 cursor-pointer text-white"
-          onClick={goToPrevious}
-        >
-          <HiOutlineChevronLeft className="h-10 w-10" />
+        className={className}
+        style={{ ...style, display: "block", right:"20px" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", left:"20px", zIndex: "1000" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  return (
+    <div className="app_slideshow">
+            <Slider {...settings}>
+                {slides.map((slide, indexSlide) => {
+                    return (
+                        <div>
+                            <div className="text-white text-4xl lg:text-8xl absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">{slide.title}</div>
+                            <img
+                                src={slide.url}
+                                alt={slide.title}
+                            />
+                        </div>
+                    )
+                })}
+            </Slider>
         </div>
-        <div
-          className="absolute top-2/4 right-0 z-50 translate-x-0 -translate-y-2/4 cursor-pointer text-white"
-          onClick={goToNext}
-        >
-          <HiOutlineChevronRight className="h-10 w-10" />
-        </div>
-      </div>
-    </div>
   );
 };
 
