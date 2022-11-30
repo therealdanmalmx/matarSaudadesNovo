9f691aaf92d10b8140d12d312af9be509a097282
therealdanmalmx <danmalmx@gmail.com>
2022-11-30 21:06:30 +0100

    Remove nexConfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 const nextTranslate = require("next-translate");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
 module.exports = {
-  nextConfig,
   env: {
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
     SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
