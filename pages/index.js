bffc33e4a310aaa76245f6cc0076e99aeb125594
therealdanmalmx <danmalmx@gmail.com>
2023-04-07 22:00:35 +0200

    Remove apollo

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-import SlideShow from '../components/SliderShow';
-import Hero from '../components/Hero';
-import Categorias from '../components/Categorias';
-import Banners from '../components/Banners';
-import About from '../components/About';
-import Newsletter from '../components/Newsletter';
-import { getCategories } from '../lib/query';
-import slides from '../utils/slides';
-import { useQuery } from 'urql';
-
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:1337/graphql',
-  cache: new InMemoryCache(),
-});
+import SlideShow from "../components/SliderShow";
+import Categorias from "../components/Categorias";
+import Banners from "../components/Banners";
+import About from "../components/About";
+import Newsletter from "../components/Newsletter";
+import { getCategories } from "../lib/query";
+import slides from "../utils/slides";
+import { useQuery } from "urql";
 
 function Home() {
   const [results] = useQuery({ query: getCategories });
@@ -37,9 +29,7 @@ function Home() {
 
   return (
     <div className="items-start justify-start text-center">
-      <ApolloProvider client={client}>
-        <Hero />
-      </ApolloProvider>
+      <SlideShow slides={slides} />
       <Categorias collections={categories} />
       <Banners />
       <About />
